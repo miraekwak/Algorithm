9f3e1dec85701ac86bcd6a6ba5f05f85921c1062
GwakMirae <alfo9490@naver.com>
2022-03-29 01:07:57 +0900

    silver5:coordinate alignment2

---
+++ 11651.py
@@ -0,0 +1,8 @@
+import sys
+N = int(input())
+numbers = []
+for _ in range(N):
+    numbers.append(list(map(int, sys.stdin.readline().split())))
+numbers.sort(key=lambda x: (x[1], x[0]))
+for number in numbers:
+    print(number[0], number[1])
