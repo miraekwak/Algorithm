c045addd2b7b044dda1cefa10a56a8427ad70bf1
GwakMirae <alfo9490@naver.com>
2022-03-31 01:24:20 +0900

    silver3: N and M(1)

--- 15649.py
+++ 15649.py
@@ -1,6 +1,22 @@
 from itertools import permutations
+
+
+def dfs(time, numbers=[]):
+    if time == 0:
+        print(*numbers, sep=" ")
+        return
+    for i in range(1, N+1):
+        if i not in numbers:
+            new_numbers = numbers.copy()
+            new_numbers.append(i)
+            dfs(time-1, new_numbers)
+
+
 if __name__ == '__main__':
     N, M = map(int, input().split())
-    numbers = [i for i in range(1, N+1)]
-    for item in permutations(numbers, M):
-        print(*item, sep=" ")
+    # code1
+    # numbers = [i for i in range(1, N+1)]
+    # for item in permutations(numbers, M):
+    #     print(*item, sep=" ")
+    # code 2
+    dfs(M)
