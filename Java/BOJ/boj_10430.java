d348f9182c9b782076be9259a39eab32e5b75436
KwakMirae <alfo9490@naver.com>
2023-04-19 14:42:07 +0900

    bronze5: 나머지

---
+++ Java/BOJ/boj_10430.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int A = Integer.valueOf(line[0]);
+        int B = Integer.valueOf(line[1]);
+        int C = Integer.valueOf(line[2]);
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
