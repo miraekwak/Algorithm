03d08e80c2573de946d08a111dc32f17dcbed8d2
KwakMirae <alfo9490@naver.com>
2023-04-19 15:44:59 +0900

    silver3: 소수구하기

---
+++ Java/BOJ/boj_1929.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_1929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] numbers = br.readLine().split(" ");
+        int M = Integer.valueOf(numbers[0]);
+        int N = Integer.valueOf(numbers[1]);
+        boolean[] prime = new boolean[N+1];
+        prime[0] = true;
+        prime[1] = true;
+        for(int i=2; i<=Math.sqrt(prime.length-1); i++) {
+            if(!prime[i]){
+                for(int j=i*i; j<prime.length; j=j+i){
+                    prime[j] = true;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=M; i<prime.length; i++) {
+            if (!prime[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+}
+
