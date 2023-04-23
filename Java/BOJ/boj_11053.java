a3c5590b8c1893efb065a1e17fdd04ed56db1668
KwakMirae <alfo9490@naver.com>
2023-04-23 16:40:06 +0900

    silver2: 가장 긴 증가하는 부분 수열

---
+++ Java/BOJ/boj_11053.java
@@ -0,0 +1,27 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class boj_11053 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.valueOf(bufferedReader.readLine());
+        int[] numbers = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(i-> Integer.valueOf(i)).toArray();
+        int[] dp = new int[N];
+        for(int i=0; i<N; i++){
+            dp[i] = 1;
+            for(int j=i-1; j>=0; j--) {
+                if(numbers[i] > numbers[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
