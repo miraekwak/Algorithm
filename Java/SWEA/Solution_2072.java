bdb1ffdb6dba7f96581b7488f6343d93bb9e4ae8
KwakMirae <alfo9490@naver.com>
2023-05-17 16:38:58 +0900

    D1: 홀수만 더하기

---
+++ Java/SWEA/Solution_2072.java
@@ -0,0 +1,25 @@
+package SWEA;
+import java.util.Scanner;
+
+class Solution_2072
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        int temp = 0;
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int sum = 0;
+            for(int i=0; i<10; i++) {
+                temp = sc.nextInt();
+                if(temp%2==1) {
+                    sum += temp;
+                }
+            }
+            System.out.println("#"+test_case+" "+sum);
+        }
+    }
+}
